2ae374cac25b63bd34b761423fd6b10d3c8d0c0d
rahmatalhakam <rahmatalhakam@gmail.com>
2022-01-17 17:53:56 +0700

    add db init

---
+++ TransactionService/Data/DbInitilizer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using TransactionService.Models;
+
+namespace TransactionService.Data
+{
+  public static class DbInitilizer
+  {
+    public static void Initilize(AppDbContext context)
+    {
+      context.Database.EnsureCreated();
+      if (context.Prices.Any())
+      {
+        return;
+      }
+      else
+      {
+        context.Prices.Add(new Price { PricePerKM = 3000, Area = "BASE" });
+        context.SaveChanges();
+      }
+
+      if (context.StatusOrders.Any())
+      {
+        return;
+      }
+      else
+      {
+        var statusOrders = new StatusOrder[]
+      {
+        new StatusOrder{Status="On Progress"},
+        new StatusOrder{Status="Finished"},
+
+      };
+        foreach (var statusOrder in statusOrders)
+        {
+          context.StatusOrders.Add(statusOrder);
+        }
+        context.SaveChanges();
+      }
+
+    }
+
+  }
+}
--- TransactionService/Program.cs
+++ TransactionService/Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TransactionService.Data;
 
 namespace TransactionService
 {
@@ -13,8 +15,29 @@ namespace TransactionService
   {
     public static void Main(string[] args)
     {
-      CreateHostBuilder(args).Build().Run();
+      var host = CreateHostBuilder(args).Build();
+      CreatedDbIfNotExists(host);
+      host.Run();
     }
+
+    private static void CreatedDbIfNotExists(IHost host)
+    {
+      using (var scope = host.Services.CreateScope())
+      {
+        var services = scope.ServiceProvider;
+        try
+        {
+          var context = services.GetRequiredService<AppDbContext>();
+          DbInitilizer.Initilize(context);
+        }
+        catch (Exception ex)
+        {
+          var logger = services.GetRequiredService<ILogger<Program>>();
+          logger.LogError(ex, "Terjadi error ketika membuat database.");
+        }
+      }
+    }
+
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
