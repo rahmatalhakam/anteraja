3b8dc1a6a943f93f04fd5ea6bdf62743bfc871ff
Dwi Cahyaningrum <dwicahyaningrum23@gmail.com>
2022-01-18 09:29:30 +0700

    add models db

---
+++ AdminService/Models/UserRole.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminService.Models
+{
+    public class UserRole
+    {
+        public string username { get; set; }
+        public string rolename { get; set; }
+    }
+}
