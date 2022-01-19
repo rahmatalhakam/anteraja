f5425bbbe1d50a4b79be5be7459fed8078ca8ff9
irvan124 <irvanmaulana124@gmail.com>
2022-01-19 12:45:17 +0700

    Modify migrate function

--- DriverService/Startup.cs
+++ DriverService/Startup.cs
@@ -112,7 +112,7 @@ namespace DriverService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AppDbContext context, DbInitializer seeder)
         {
-            context.Database.Migrate();
+            context.Database.EnsureCreated();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
