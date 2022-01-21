3fef15bb27e20b86fd65c468684b6030a11166c8
irvan124 <irvanmaulana124@gmail.com>
2022-01-21 22:36:47 +0700

    Creating Accept Order Functions

--- DriverService/Data/Orders/IOrder.cs
+++ DriverService/Data/Orders/IOrder.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using DriverService.Dtos.Orders;
 using DriverService.Models;
 
 namespace DriverService.Data.Orders
 {
-  public interface IOrder
-  {
+    public interface IOrder
+    {
 
-    Task<Order> Insert(Order user);
-    Task<IEnumerable<Order>> GetOrders();
+        Task<Order> Insert(Order user);
+        Task<IEnumerable<Order>> GetOrders();
+        Task<Order> GetOrderById(int id);
+        Task<Order> AcceptOrder(OrderInput input);
 
-  }
+    }
 }
