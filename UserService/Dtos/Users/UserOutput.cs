ff2746c99aea66c298f65a81154b04e4c7173c63
rahmatalhakam <rahmatalhakam@gmail.com>
2022-01-22 22:28:25 +0700

    bug fixes on admin, driver, adn user service

---
+++ UserService/Dtos/Users/UserOutput.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace UserService.Dtos.Users
+{
+  public class UserOutput
+  {
+    public string Id { get; set; }
+    public string Username { get; set; }
+  }
+}
